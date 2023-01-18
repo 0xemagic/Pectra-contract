f4a082d770f9efb3c00a7436aaca6fc0308ece3b
0xemagic <skrullweb.dev@gmail.com>
2023-01-18 22:55:53 +0530

    Add Tabs

--- pages/trade.tsx
+++ pages/trade.tsx
@@ -8,6 +8,7 @@ import {
   TabList,
   Tabs,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 
 const Trade = () => {
@@ -91,7 +92,8 @@ const Trade = () => {
           >
             Pick a pair
           </Text>
-          <Box
+         <VStack gap='0.825rem'>
+         <Box
             bg="rgba(172, 224, 117, 0.2)"
             w="full"
             borderColor="#ACE075"
@@ -130,6 +132,75 @@ const Trade = () => {
               </Flex>
             </Flex>
           </Box>
+          <Box
+            bg="rgba(255, 114, 114, 0.2)"
+            w="full"
+            borderColor="#FF7272"
+            borderWidth="2px"
+            borderRadius="7px"
+            py="0.875rem"
+            px="1.25rem"
+          >
+            <Flex
+              fontFamily="body"
+              justify="space-between"
+              alignItems="center"
+              w="full"
+            >
+              <Text fontWeight={600} fontFamily="heading" fontSize="0.9rem">
+                Short
+              </Text>
+              <Flex gap="0.75rem" flexDir="column" justifyContent="flex-end">
+                <Select
+                  fontWeight={600}
+                  fontSize="1.01rem"
+                  w="fit-content"
+                  ml="auto"
+                  mr={0}
+                  variant="unstyled"
+                  placeholder={"0.07 BTC"}
+                >
+                  <option value="1 eth">0.07 BTC</option>
+                </Select>
+                <Flex ml="auto" mr={0} fontSize="0.875rem">
+                  <Text mr={2} fontWeight={300}>
+                    current price:
+                  </Text>
+                  <Text fontWeight={600}>$1600</Text>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Box>
+          <Box
+            bg="#171717"
+            w="full"
+            borderColor="rgba(255, 255, 255, 0.2)"
+            borderWidth="2px"
+            borderRadius="7px"
+            py="0.875rem"
+            px="1.25rem"
+          >
+            <Flex
+              fontFamily="body"
+              justify="space-between"
+              alignItems="center"
+              w="full"
+            >
+              <Text fontWeight={600} fontFamily="heading" fontSize="0.9rem">
+                Amount
+              </Text>
+              <Flex gap="0.75rem" flexDir="column" justifyContent="flex-end">
+                <Text
+                  fontWeight={600}
+                  fontSize="1.01rem"
+                  >
+1200 USDC
+                </Text>
+                
+              </Flex>
+            </Flex>
+          </Box>
+         </VStack>
         </GridItem>
 
         <GridItem
