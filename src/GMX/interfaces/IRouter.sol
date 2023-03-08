7b257fc76954898e17d235c9ba168c1830b8bfe4
0xemagic <skrullweb.dev@gmail.com>
2023-03-09 00:37:50 +0500

    Increase position impl

--- src/GMX/interfaces/IRouter.sol
+++ src/GMX/interfaces/IRouter.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.13;
 
 interface IRouter {
+    function approvePlugin(address _plugin) external;
+
     function swap(address[] memory _path, uint256 _amountIn, uint256 _minOut, address _receiver) external;
     
     function swapETHToTokens(address[] memory _path, uint256 _minOut, address _receiver) external payable;
--- src/PEAR/GmxAdapter.sol
+++ src/PEAR/GmxAdapter.sol
@@ -7,15 +7,53 @@ import "../GMX/interfaces/IPositionRouter.sol";
 
 contract GMXAdapter {
 
+    address public OWNER;
     address public ROUTER;
     address public POSITION_ROUTER;
-
+    
     constructor(address _router, address _positionRouter) {
+        OWNER = msg.sender;
         ROUTER = _router;
         POSITION_ROUTER = _positionRouter;
     }
 
-    function approve(address token, address spender, uint256 amount) external returns (bool) {
+    modifier onlyOwner() {
+        require(OWNER == msg.sender, "caller is not the owner");
+        _;
+    }
+
+    function approve(address token, address spender, uint256 amount) external onlyOwner returns (bool) {
         return IERC20(token).approve(spender, amount);
+    }
+
+    function approvePlugin(address _plugin) external onlyOwner {
+        IRouter(ROUTER).approvePlugin(_plugin);
+    }
+
+    function createIncreasePosition(
+        address[] memory _path,
+        address _indexToken,
+        uint256 _amountIn,
+        uint256 _minOut,
+        uint256 _sizeDelta,
+        bool _isLong,
+        uint256 _acceptablePrice,
+        uint256 _executionFee,
+        bytes32 _referralCode,
+        address _callbackTarget
+    ) external payable onlyOwner returns (bytes32) {
+        return IPositionRouter(POSITION_ROUTER).createIncreasePosition(_path, _indexToken, _amountIn, _minOut, _sizeDelta, _isLong, _acceptablePrice, _executionFee, _referralCode, _callbackTarget);
+    }
+
+    function cancelIncreasePosition(bytes32 _key, address payable _executionFeeReceiver) external onlyOwner returns (bool) {
+        return IPositionRouter(POSITION_ROUTER).cancelIncreasePosition(_key, _executionFeeReceiver);
+    }
+
+    function executeIncreasePosition(bytes32 _key, address payable _executionFeeReceiver) external onlyOwner returns (bool) {
+        return IPositionRouter(POSITION_ROUTER).executeIncreasePosition(_key, _executionFeeReceiver);
+    }
+
+    function withdrawToken(address token, address to, uint256 amount) external onlyOwner returns (bool) {
+        return IERC20(token).transfer(to, amount);
     }
 }
