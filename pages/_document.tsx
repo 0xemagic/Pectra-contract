9af7efc8c39b81bc31cbd0bd1ee0c552069e3fdf
0xemagic <skrullweb.dev@gmail.com>
2023-03-22 17:10:22 -0300

    changed fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,50 @@ import theme from '../styles/theme'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link
+            rel="preload"
+            href="/fonts/IntegralCF-Regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+           <link
+            rel="preload"
+            href="/fonts/IntegralCF-RegularOblique.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+            <link
+            rel="preload"
+            href="/fonts/IntegralCF-Bold.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+            <link
+            rel="preload"
+            href="/fonts/IntegralCF-ExtraBold.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+            <link
+            rel="preload"
+            href="/fonts/IntegralCF-Heavy.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+            <link
+            rel="preload"
+            href="/fonts/IntegralCF-Medium.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+       </Head> 
       <body>
         <Main />
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
