7bf456aea50611cee9ee23f8e2a531ba8afeaaf1
0xemagic <skrullweb.dev@gmail.com>
2023-03-29 15:36:27 +0200

    Factory v1 implementation

--- src/Adapters/GmxAdapter.sol
+++ src/Adapters/GmxAdapter.sol
@@ -5,13 +5,15 @@ import "../GMX/interfaces/IERC20.sol";
 import "../GMX/interfaces/IRouter.sol";
 import "../GMX/interfaces/IPositionRouter.sol";
 
-contract GMXAdapter {
-
+contract GMXAdapter is {
     address public OWNER;
     address public ROUTER;
     address public POSITION_ROUTER;
     
-    constructor(address _router, address _positionRouter) {
+    constructor(
+        address _router,
+        address _positionRouter
+    ) {
         OWNER = msg.sender;
         ROUTER = _router;
         POSITION_ROUTER = _positionRouter;
@@ -57,7 +59,7 @@ contract GMXAdapter {
         uint256 _executionFee,
         bytes32 _referralCode,
         address _callbackTarget
-    ) external payable returns (bytes32) {
+    ) external payable onlyOwner returns (bytes32) {
         return IPositionRouter(POSITION_ROUTER).createIncreasePositionETH{value: msg.value}(_path, _indexToken, _minOut, _sizeDelta, _isLong, _acceptablePrice, _executionFee, _referralCode, _callbackTarget);
     }
     
@@ -73,7 +75,7 @@ contract GMXAdapter {
         uint256 _executionFee,
         bool _withdrawETH,
         address _callbackTarget
-    ) external payable returns (bytes32) {
+    ) external payable onlyOwner returns (bytes32) {
         return IPositionRouter(POSITION_ROUTER).createDecreasePosition(_path, _indexToken, _collateralDelta, _sizeDelta, _isLong, _receiver, _acceptablePrice, _minOut, _executionFee, _withdrawETH, _callbackTarget);
     }
     
