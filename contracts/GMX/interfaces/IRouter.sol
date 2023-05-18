50f8b3d22485a74d611cdee3b9f042299a3d28f0
0xemagic <skrullweb.dev@gmail.com>
2023-05-18 16:02:35 +0500

    Filter router swap functions

--- contracts/GMX/interfaces/IRouter.sol
+++ contracts/GMX/interfaces/IRouter.sol
@@ -3,10 +3,4 @@ pragma solidity ^0.8.13;
 
 interface IRouter {
     function approvePlugin(address _plugin) external;
-
-    function swap(address[] memory _path, uint256 _amountIn, uint256 _minOut, address _receiver) external;
-    
-    function swapETHToTokens(address[] memory _path, uint256 _minOut, address _receiver) external payable;
-    
-    function swapTokensToETH(address[] memory _path, uint256 _amountIn, uint256 _minOut, address payable _receiver) external;
 }
