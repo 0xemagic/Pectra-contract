21d87707bc0a46db14d5aa4be32077e7d605c674
0xemagic <skrullweb.dev@gmail.com>
2023-06-22 15:03:35 +0500

    :mag: testing: completed test cases for the gmx factory contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,14 +8,16 @@ config({ path: resolve(__dirname, "./.env") });
 
 module.exports = {
   solidity: {
-    version: "0.8.17",
+    compilers: [{
+      version: "0.8.17"
+    }, { version: "0.6.12" }, { version: "0.6.0" }],
     settings: {
       metadata: {
         bytecodeHash: "none",
       },
       optimizer: {
         enabled: true,
-        runs: 2,
+        runs: 200,
       },
     }
   },
@@ -29,8 +31,17 @@ module.exports = {
     polygonMumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_ADMIN]
+    },
+    arbitrumGoerli: {
+      url: "https://goerli-rollup.arbitrum.io/rpc",
+      chainId: 421613,
       accounts: [process.env.PRIVATE_KEY]
     },
+    arbitrumOne: {
+      url: "https://arb1.arbitrum.io/rpc",
+      accounts: [process.env.PRIVATE_KEY_ADMIN]
+    }
   },
   etherscan: {
     apiKey: {
--- test/Gmx.js
+++ test/Gmx.js
@@ -1,169 +1,166 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
-describe("GMXFactory", function () {
+describe("GMXFactory", async function () {
     let gmxFactory;
     let deployer;
     let tokenPath;
+    let positionKeeper;
+    let vault;
+    let _acceptablePriceLong;
+    let _acceptablePriceShort;
 
-    const _router = "0x3D69F5E48fE3417422727329149152FcCF1D9A1b";
-    const _positionRouter = "0x2F7f73056B72C46a6DA51a46ad29453F6a727874";
     const _tokenPath = "0xAd70f9404504d74C1453dB9A9DdF6d974361914E";
     const _indexToken = "0x75Bd7A9086447e4D5afB06B30C13C14Bf94060eB";
-    const _sizeDelta = ethers.utils.parseEther("40");
-    const _acceptablePrice = ethers.utils.parseEther("1831.57");
-    const _acceptablePriceShort = ethers.utils.parseEther("1811.57");
+    const _positionKeeper = "0x38030cAe976a82Cf17e4b89522e339df3e514a79";
+    const _gmxFactory = "0x5aaa56ef3695ac3dc969EaA31F0C75e511c4f194";
+    const _vault = "0x34B20Eb9E1e4b7D4C683963855DAE608be5529ab";
+
+    //Assuming that user will long 10 USDC with 5x Leverage
+    const amountIn = ethers.utils.parseEther("10");
+    const _sizeDelta = ethers.utils.parseUnits("50", 30);
 
     before(async function () {
         const GMXFactory = await ethers.getContractFactory("GMXFactory");
         const TokenPath = await ethers.getContractFactory("Token");
+        const PositionKeeper = await ethers.getContractFactory("PositionKeeper");
+        const Vault = await ethers.getContractFactory("Vault");
+
         const signer = await ethers.getSigners();
         deployer = signer[0];
-        gmxFactory = await GMXFactory.deploy(_router, _positionRouter);
-        await gmxFactory.deployed();
-        console.log("GMX Factory deployed to:", gmxFactory.address);
+        admin = signer[1];
+
+        gmxFactory = await GMXFactory.attach(_gmxFactory);
         tokenPath = await TokenPath.attach(_tokenPath);
+        positionKeeper = await PositionKeeper.attach(_positionKeeper);
+        vault = await Vault.attach(_vault);
+
+        const _acceptablePrice = await vault.getMaxPrice(_indexToken);
+
+        // Adjust the Prices by 1% for Acceptable Price for Long and Short
+        const onePercent = ethers.utils.parseUnits("0.01", 18);
+        _acceptablePriceLong = _acceptablePrice.mul(ethers.utils.parseUnits("101", 2)).div(ethers.utils.parseUnits("100", 2));
+        _acceptablePriceShort = _acceptablePrice.mul(ethers.utils.parseUnits("99", 2)).div(ethers.utils.parseUnits("100", 2));
+
     });
 
     it("should create a long positions", async function () {
-        const amountIn = ethers.utils.parseEther("10");
+        this.timeout(120000);
         const value = ethers.utils.parseEther("0.00018");
         const minOut = ethers.utils.parseEther("0");
+        const initialPosition = await gmxFactory.positions(deployer.address);
 
         // Create the long position
         approvalTx = await tokenPath.connect(deployer).approve(gmxFactory.address, amountIn);
-        await gmxFactory.connect(deployer).openLongPosition(
-            [_tokenPath],
+        await approvalTx.wait(3);
+        
+        const creatingPositionTx = await gmxFactory.connect(deployer).openLongPosition(
+            [_tokenPath, _indexToken],
             _indexToken,
             amountIn,
             minOut,
             _sizeDelta,
-            _acceptablePrice,
+            _acceptablePriceLong,
             { value: value }
         );
+        await creatingPositionTx.wait(10);
 
         // Get the number of positions for the deployer
         const positions = await gmxFactory.positions(deployer.address);
 
         // Assert that the number of positions is 1
-        expect(positions).to.equal(0);
+        expect(positions).to.equal(Number(initialPosition) + 1);
 
     });
 
-    it("should create two long positions for one user", async function () {
-        const amountIn = ethers.utils.parseEther("10");
-        const value = ethers.utils.parseEther("0.00018");
-        const minOut = ethers.utils.parseEther("0");
+    it("should verify the position we create", async function () {
 
-        // Create the first long position
-        await tokenPath.connect(deployer).approve(gmxFactory.address, amountIn);
-        const creatinglong = await gmxFactory.connect(deployer).openLongPosition(
-            [_tokenPath],
-            _indexToken,
-            amountIn,
-            minOut,
-            _sizeDelta,
-            _acceptablePrice,
-            { value: value }
-        );
-        console.log("Creating long position:", creatinglong);
-
-        // Create the second long position
-        await tokenPath.connect(deployer).approve(gmxFactory.address, amountIn);
-        await gmxFactory.connect(deployer).openLongPosition(
-            [_tokenPath],
-            _indexToken,
-            amountIn,
-            minOut,
-            _sizeDelta,
-            _acceptablePrice,
-            { value: value }
-        );
-
-        // Get the number of positions for the deployer
         const positions = await gmxFactory.positions(deployer.address);
 
-        // Assert that the number of positions is 2
-        expect(positions).to.equal(2);
-    });
-
-    it("should return position Ids for the positions we create", async function () {
-        const amountIn = ethers.utils.parseEther("10");
-        const value = ethers.utils.parseEther("0.00018");
-        const minOut = ethers.utils.parseEther("0");
-
-        // Create the long position
-        approvalTx = await tokenPath.connect(deployer).approve(gmxFactory.address, amountIn);
-        await gmxFactory.connect(deployer).openLongPosition(
-            [_tokenPath],
-            _indexToken,
-            amountIn,
-            minOut,
-            _sizeDelta,
-            _acceptablePrice,
-            { value: value }
-        );
-
         // Get the number of positions for the deployer
-        const positionId = await gmxFactory.indexedPositions(deployer.address, 1);
+        const positionId = await gmxFactory.indexedPositions(deployer.address, positions);
 
         // Assert that the positionId is not Empty
         expect(positionId).to.not.equal(0);
 
     });
 
-    it("should close a position we created", async function () {
-        const amountIn = ethers.utils.parseEther("10");
+    it("should execute Long position", async function () {
+        this.timeout(120000);
+
+        //Executes the Position
+        const executionTx = await positionKeeper.connect(admin).execute()
+        await executionTx.wait(20);
+
+    });
+
+    it("should not close position if caller is not position keeper", async function () {
+        const positionId = await gmxFactory.indexedPositions("0x41209c58D558d017b481f04C5bB1ea7EF34ea97B", 1);
+        expect(positionId).to.be.revertedWith("not a position owner");
+    })
+
+    it("should close a Long position we created", async function () {
+        const value = ethers.utils.parseEther("0.00018");
+        const positions = await gmxFactory.positions(deployer.address);
+
+        // Get the number of positions for the deployer
+        const positionId = await gmxFactory.indexedPositions(deployer.address, positions);
+
+        const closeTx = await gmxFactory.closePosition(positionId, [_indexToken], _acceptablePriceLong, true, { value: value });
+        await closeTx.wait(5);
+        expect(closeTx.status).to.equal(1);
+
+    });
+
+    it("should create a short positions", async function () {
+        this.timeout(120000);
+
         const value = ethers.utils.parseEther("0.00018");
         const minOut = ethers.utils.parseEther("0");
-        const initialBalance = await deployer.getBalance();
+        const initialPosition = await gmxFactory.positions(deployer.address);
 
-        // Create the long position
+        // Create the short position
         approvalTx = await tokenPath.connect(deployer).approve(gmxFactory.address, amountIn);
-        await gmxFactory.connect(deployer).openLongPosition(
-            [_tokenPath],
+        await approvalTx.wait(3);
+
+        const creatingPositionTx = await gmxFactory.connect(deployer).openShortPosition(
+            [_tokenPath, _indexToken],
             _indexToken,
             amountIn,
             minOut,
             _sizeDelta,
-            _acceptablePrice,
+            _acceptablePriceShort,
             { value: value }
         );
+        await creatingPositionTx.wait(10);
 
-        // Get the number of positions for the deployer
-        const positionId = await gmxFactory.indexedPositions(deployer.address, 1);
-
-        await gmxFactory.closePosition(positionId, [_tokenPath], _acceptablePrice, true);
-
-        //Assert that balance should be increased
-        const finalBalance = await deployer.getBalance();
-        expect(finalBalance).to.be.gt(initialBalance);
-
-    });
-
-    it("should open a short position", async function () {
-        const amountIn = ethers.utils.parseEther("10");
-        const value = ethers.utils.parseEther("0.00018");
-        const minOut = ethers.utils.parseEther("0");
-    
-        // Create the short position
-        await tokenPath.connect(deployer).approve(gmxFactory.address, amountIn);
-        await gmxFactory.connect(deployer).openShortPosition(
-          [_tokenPath],
-          _indexToken,
-          amountIn,
-          minOut,
-          _sizeDelta,
-          _acceptablePriceShort,
-          { value: value }
-        );
-    
         // Get the number of positions for the deployer
         const positions = await gmxFactory.positions(deployer.address);
-        console.log("Number of positions for the deployer:", positions);
-    
-        // Assert that the number of positions is 3
-        expect(positions).to.equal(3);
-      });
+
+        // Assert that the number of positions is 1")
+        expect(positions).to.equal(Number(initialPosition) + 1);
+    });
+
+    it("should execute Short position", async function () {
+        this.timeout(120000);
+
+        //Executes the Position
+        const executionTx = await positionKeeper.connect(admin).execute()
+        await executionTx.wait(20);
+
+    })
+
+    it("should close a Short position we created", async function () {
+        const value = ethers.utils.parseEther("0.00018");
+        const positions = await gmxFactory.positions(deployer.address);
+
+        // Get the number of positions for the deployer
+        const positionId = await gmxFactory.indexedPositions(deployer.address, positions);
+
+        const closeTx = await gmxFactory.closePosition(positionId, [_indexToken], _acceptablePriceShort, true, { value: value });
+        await closeTx.wait(5);
+        expect(closeTx.status).to.equal(1);
+
+    });
 
 });
