b7f9069c8d01531662da278b0d70c68b623de9d6
0xemagic <skrullweb.dev@gmail.com>
2023-07-10 17:42:51 +0500

    :fire: feature: added support of hardhat testcases in foundry

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,57 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-foundry");
+require("@nomicfoundation/hardhat-toolbox");
+
+const { resolve } = require("path");
+const { config } = require("dotenv");
+
+config({ path: resolve(__dirname, "./.env") });
 
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [{
+      version: "0.8.19"
+    }, { version: "0.6.12" }, { version: "0.6.0" }],
+    settings: {
+      metadata: {
+        bytecodeHash: "none",
+      },
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    }
+  },
+  networks: {
+    localhost: {
+      timeout: 120000
+    },
+    hardhat: {
+      allowUnlimitedContractSize: true
+    },
+    polygonMumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_ADMIN]
+    },
+    arbitrumGoerli: {
+      url: "https://goerli-rollup.arbitrum.io/rpc",
+      chainId: 421613,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    arbitrumOne: {
+      url: "https://arb1.arbitrum.io/rpc",
+      accounts: [process.env.PRIVATE_KEY_MAINNET]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+    }
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
 };
